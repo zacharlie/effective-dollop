df247a4b27836c12e938e9ce2c97853717654238
Charlie <64078329+zacharlie@users.noreply.github.com>
2021-09-11 16:20:41 +0200

    Update gdal-file-loop.cmd

--- utils/gdal-file-loop.cmd
+++ utils/gdal-file-loop.cmd
@@ -1,5 +1,4 @@
 @echo off
-@rem can't seem to find a way to merge gpkg rasters after the fact. Leaving the logic for now
 @setlocal enableextensions
 @cd /d "%~dp0"
 
@@ -8,12 +7,9 @@ set PATH=%PATH%%~dp0bin;
 call C:\OSGeo4W\bin\o4w_env.bat;
 
 set INDIR=%~dp0input_data
-set TMPPATH=%~dp0tmp
 rem set OUTFILE=%~dp0output.gpkg
 REM Specify the file extension for input files
 set EXT=tif
-
-if not exist "%TMPPATH%" (mkdir %TMPPATH%)
 
 if exist "%OUTFILE%" (
     if exist "%OUTFILE%.old" (
@@ -27,10 +23,7 @@ if exist "%OUTFILE%" (
 cd "%INDIR%"
 
 for %%f in (*.%EXT%) do (
-    rem gdal_translate -of gpkg "%%f" "%OUTFILE%" -co "RASTER_TABLE=%%~nf"
-    gdal_translate -of gpkg "%%f" "%TMPPATH%\%%~nf.gpkg" -co TILE_FORMAT=AUTO
-    rem gdalmanage copy -f gpkg "%OUTFILE%"  "%TMPPATH%\%%~nf.gpkg" -co "RASTER_TABLE=%%~nf"
-    rem del /f "%TMPPATH%\%%~nf.gpkg"
+    gdal_translate -of gpkg "%%f" "%OUTFILE%" -co TILE_FORMAT=AUTO -co APPEND_SUBDATASET=YES  -co RASTER_TABLE=%%~nf
     echo "Processed %%~nf..."
 )
 
